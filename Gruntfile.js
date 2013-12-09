9ad6df93447b997af219165aa014ea376b442289
寒泉 <hanquan.csf@taobao.com>
2013-12-09 10:38:37 +0800

    remove css load

--- Gruntfile.js
+++ Gruntfile.js
@@ -82,8 +82,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
 
