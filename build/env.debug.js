7735de341dfe853980029b0d5b354ea429ffeb53
zhuxun.jb <zhuxun.jb@taobao.com>
2013-12-17 19:48:25 +0800

    更新windvane版本对应的主客版本

--- build/env.debug.js
+++ build/env.debug.js
@@ -90,25 +90,38 @@
 ;
 (function(window, lib) {
     lib.env = lib.env || {};
-    
-    var ttid = lib.env.param['ttid'];
+
     var ua = window.navigator.userAgent;
-    var reg = /@taobao_(iphone|android|ipad)_([\d\.]+)/;
-    var uaMatched = ua.match(reg);
-    var ttidMatched = ttid && ttid.match(reg);
-
-    if(uaMatched || ttidMatched) {
-
-        lib.env.taobaoApp = {
-            version: uaMatched[2] || ttidMatched[2],
-            platform: (uaMatched[1] || uaMatched[1]).replace(/^ip/, 'iP').replace(/^a/, 'A')
+    
+    if(ua.match(/Android ([\d\.]+)/)) {
+        lib.env.os = {
+            name: 'Android',
+            isAndroid: true,
+            version: RegExp.$1
         }
+    } else if(ua.match(/(iPhone|iPad|iPod)/)) {
+        var name = RegExp.$1;
 
-        if(lib.version) {
-            lib.env.taobaoApp.version = lib.version(lib.env.taobaoApp.version);
+        ua.match(/OS ([\d_]+) like Mac OS X/);
+
+        lib.env.os = {
+            name: name,
+            isIPhone: (name === 'iPhone' || name === 'iPod'),
+            isIPad: name === 'iPad',
+            isIOS: true,
+            version: RegExp.$1.split('_').join('.')
+        }
+    } else {
+        lib.env.os = {
+            name:'unknown',
+            version:'0.0.0'
         }
     }
-
+    
+    if(lib.version) {
+        lib.env.os.version = lib.version(lib.env.os.version);
+    }
+    
 })(window, window['lib'] || (window['lib'] = {}));
 ;
 (function(window, lib) {
@@ -172,36 +185,61 @@
 ;
 (function(window, lib) {
     lib.env = lib.env || {};
-
+    
+    var ttid = lib.env.param['ttid'];
     var ua = window.navigator.userAgent;
-    
-    if(ua.match(/Android ([\d\.]+)/)) {
-        lib.env.os = {
-            name: 'Android',
-            isAndroid: true,
-            version: RegExp.$1
-        }
-    } else if(ua.match(/(iPhone|iPad|iPod)/)) {
-        var name = RegExp.$1;
+    var ttidReg = /@taobao_(iphone|android|ipad)_([\d\.]+)/;
+    var wvMatched = ua.match(/WindVane[\/\s]([\d\.\_]+)/);
+    var uaMatched = ua.match(ttidReg);
+    var ttidMatched = ttid && ttid.match(ttidReg);
+    var wvVersion;
+    var appVersion;
+    var platform = lib.os.name;
+    var app;
 
-        ua.match(/OS ([\d_]+) like Mac OS X/);
+    if (wvMatched && wvMatched[1]) {
+        wvVersion = wvMatched[1];
+    }
 
-        lib.env.os = {
-            name: name,
-            isIPhone: (name === 'iPhone' || name === 'iPod'),
-            isIPad: name === 'iPad',
-            isIOS: true,
-            version: RegExp.$1.split('_').join('.')
-        }
+    if (uaMatched && uaMatched[1]) {
+        platform = uaMatched[1];
+    } else if (ttidMatched && ttidMatched[1]) {
+        platform = ttidMatched[1];
+    }
+
+    if (uaMatched && uaMatched[2]) {
+        appVersion = uaMatched[2];
+    } else if (ttidMatched && ttidMatched[2]) {
+        appVersion = ttidMatched[2];
     } else {
-        lib.env.os = {
-            name:'unknown',
-            version:'0.0.0'
+        if (lib.os.isAndroid) {
+            if (['2.5.1', '2.5.2', '2.5.3', '2.5.4', '2.5.5'].indexOf(wvVersion) > -1) {
+                appVersion = '3.9.2';
+            } else if (wvVersion === '2.5.6') {
+                appVersion = '3.9.3';
+            } else if (wvVersion === '2.6.0') {
+                appVersion = '3.9.5';
+            }
+        } else if (lib.os.isIOS) {
+            if (parseFloat(wvVersion) >= 2.5 && parseFloat(wvVersion) < 2.6) {
+                appVersion = '3.4.0';
+            } else if (wvVersion === '2.6.0') {
+                appVersion = '3.4.5';
+            }
         }
     }
-    
-    if(lib.version) {
-        lib.env.os.version = lib.version(lib.env.os.version);
+
+    if (appVersion) {
+        app = lib.env.taobaoApp = {
+            version: appVersion,
+            windvine: wvVersion,
+            platform: platform
+        }
     }
-    
+
+    if (lib.version && app) {
+        app.version = lib.version(app.version);
+        app.windvine = lib.version(app.windvine);
+    }
+
 })(window, window['lib'] || (window['lib'] = {}));
--- src/app.js
+++ src/app.js
@@ -4,20 +4,58 @@
     
     var ttid = lib.env.param['ttid'];
     var ua = window.navigator.userAgent;
-    var reg = /@taobao_(iphone|android|ipad)_([\d\.]+)/;
-    var uaMatched = ua.match(reg);
-    var ttidMatched = ttid && ttid.match(reg);
+    var ttidReg = /@taobao_(iphone|android|ipad)_([\d\.]+)/;
+    var wvMatched = ua.match(/WindVane[\/\s]([\d\.\_]+)/);
+    var uaMatched = ua.match(ttidReg);
+    var ttidMatched = ttid && ttid.match(ttidReg);
+    var wvVersion;
+    var appVersion;
+    var platform = lib.os.name;
+    var app;
 
-    if(uaMatched || ttidMatched) {
+    if (wvMatched && wvMatched[1]) {
+        wvVersion = wvMatched[1];
+    }
 
-        lib.env.taobaoApp = {
-            version: uaMatched[2] || ttidMatched[2],
-            platform: (uaMatched[1] || uaMatched[1]).replace(/^ip/, 'iP').replace(/^a/, 'A')
+    if (uaMatched && uaMatched[1]) {
+        platform = uaMatched[1];
+    } else if (ttidMatched && ttidMatched[1]) {
+        platform = ttidMatched[1];
+    }
+
+    if (uaMatched && uaMatched[2]) {
+        appVersion = uaMatched[2];
+    } else if (ttidMatched && ttidMatched[2]) {
+        appVersion = ttidMatched[2];
+    } else {
+        if (lib.os.isAndroid) {
+            if (['2.5.1', '2.5.2', '2.5.3', '2.5.4', '2.5.5'].indexOf(wvVersion) > -1) {
+                appVersion = '3.9.2';
+            } else if (wvVersion === '2.5.6') {
+                appVersion = '3.9.3';
+            } else if (wvVersion === '2.6.0') {
+                appVersion = '3.9.5';
+            }
+        } else if (lib.os.isIOS) {
+            if (parseFloat(wvVersion) >= 2.5 && parseFloat(wvVersion) < 2.6) {
+                appVersion = '3.4.0';
+            } else if (wvVersion === '2.6.0') {
+                appVersion = '3.4.5';
+            }
         }
+    }
 
-        if(lib.version) {
-            lib.env.taobaoApp.version = lib.version(lib.env.taobaoApp.version);
+    if (appVersion) {
+        app = lib.env.taobaoApp = {
+            version: appVersion,
+            windvine: wvVersion,
+            platform: platform
         }
+    }
+
+    if (lib.version && app) {
+        app.version = lib.version(app.version);
+        app.windvine = lib.version(app.windvine);
     }
 
 })(window, window['lib'] || (window['lib'] = {}));
