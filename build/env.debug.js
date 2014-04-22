5ff6af87aaad4f430c669409774759b4da5bdeab
zhuxun.jb <zhuxun.jb@taobao.com>
2014-04-22 18:18:37 +0800

    update

--- build/env.debug.js
+++ build/env.debug.js
@@ -201,10 +201,10 @@
     var ttid = lib.env.params['ttid'];
     var ua = window.navigator.userAgent;
 
-    var windvine;
+    var windvane;
     var matched;
     if ((matched = ua.match(/WindVane[\/\s]([\d\.\_]+)/))) {
-        windvine = matched[1];
+        windvane = matched[1];
     }
 
     var appname;
@@ -232,23 +232,23 @@
         appname = matched[1];
         platform = matched[2].replace(/^ip/, 'iP').replace(/^a/, 'A');
         version = matched[3];
-    } else if (windvine) {
-        windvine = lib.version(windvine);
+    } else if (windvane) {
+        windvane = lib.version(windvane);
         platform = lib.env.os.name;
         appname = 'taobao';
 
         if (lib.env.os.isAndroid) {
-            if (windvine.gte('2.5.1') && windvine.lte('2.5.5')) {
+            if (windvane.gte('2.5.1') && windvane.lte('2.5.5')) {
                 version = '3.9.2';
-            } else if (windvine.eq('2.5.6')) {
+            } else if (windvane.eq('2.5.6')) {
                 version = '3.9.3';
-            } else if (windvine.eq('2.6.0')) {
+            } else if (windvane.eq('2.6.0')) {
                 version = '3.9.5';
             }
         } else if (lib.env.os.isIOS) {
-            if (windvine.gte('2.5.0') && windvine.lt('2.6.0')) {
+            if (windvane.gte('2.5.0') && windvane.lt('2.6.0')) {
                 version = '3.4.0';
-            } else if (windvine.eq('2.6.0')) {
+            } else if (windvane.eq('2.6.0')) {
                 version = '3.4.5';
             }
         }
@@ -256,7 +256,7 @@
 
     if (appname && platform && version) {
         lib.env.taobaoApp = {
-            windvine: lib.version(windvine || '0.0.0'),
+            windvane: lib.version(windvane || '0.0.0'),
             appname: appname,
             version: lib.version(version || '0.0.0'),
             platform: platform
